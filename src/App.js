7f95631b26d078f77881ce83c6d0563dc0f18fdf
Michal Boczula <michal.boczula0101@gmail.com>
2024-06-27 21:59:02 +0200

    Initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { Provider, useSelector, useDispatch } from 'react-redux';
+import store from './stores/BasicStore';
+import { Button } from 'primereact/button';
+import { increment, decrement } from './reducers/CounterReducer';
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
+function Counter() {
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h2>Counter: {count}</h2>
+      <Button label="Increment" icon="pi pi-plus" onClick={() => dispatch(increment())} />
+      <Button label="Decrement" icon="pi pi-minus" onClick={() => dispatch(decrement())} />
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <h1>Welcome</h1>
+        <Counter />
+      </div>
+    </Provider>
   );
 }
 
